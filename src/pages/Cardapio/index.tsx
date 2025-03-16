fb5ea910673b50d40d16feae30be589e7305d15a
Leonardo Luz <leonardoluz10@hotmail.com>
2025-03-16 16:47:19 -0300

    Inicializando projeto aluroni-ts

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./Cardapio.module.scss";
+
+export default function Cardapio() {
+  return (<main className={styles.cardapio}>
+    <nav className="menu">Navegar</nav>
+  </main>);
+}
