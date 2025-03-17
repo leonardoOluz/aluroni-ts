8c56146db0556a8122bfcbf385cab2a610a835a1
Leonardo Luz <leonardoluz10@hotmail.com>
2025-03-17 15:36:09 -0300

    feat: :white_check_mark: Feat: Criando arquivos na pasta styles para breakpoints e variaveis, criado componente Bucador e integrou ao componente Cardapio  em pages.

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@components": path.resolve(__dirname, "./src/components"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
     }
   },
   plugins: [react(), svgr()],
